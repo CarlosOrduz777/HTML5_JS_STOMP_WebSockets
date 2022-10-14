17e426d87d7342efe82c5f0349e6e43ac6bfea64
CarlosOrduz777 <carlos.orduz@mail.escuelaing.edu.co>
2022-10-13 19:48:31 -0500

    PARTE FINAL

--- src/main/java/edu/eci/arsw/collabpaint/controller/PointController.java
+++ src/main/java/edu/eci/arsw/collabpaint/controller/PointController.java
@@ -2,16 +2,48 @@ package edu.eci.arsw.collabpaint.controller;
 
 
 import edu.eci.arsw.collabpaint.model.Point;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 public class PointController {
 
-    @MessageMapping("/newpoint")
-    @SendTo("/topic/newpoint")
-    public Point addPoint(Point p){
-        return new Point(p.getX(),p.getY());
+    @Autowired
+    SimpMessagingTemplate msgt;
+
+    Map<String, ArrayList<Point>> poligons = new ConcurrentHashMap<>();
+
+    @MessageMapping("/newpoint.{numdibujo}")
+    public Point handlePointEvent(Point pt,@DestinationVariable String numdibujo) throws Exception {
+        System.out.println("Nuevo punto recibido en el servidor!:"+pt);
+        msgt.convertAndSend("/topic/newpoint"+numdibujo, pt);
+        if(poligons.containsKey(numdibujo)){
+            poligons.get(numdibujo).add(pt);
+            if(poligons.get(numdibujo).size() >= 3){
+                msgt.convertAndSend("/topic/newpolygon."+numdibujo, poligons.get(numdibujo));
+            }
+        }else {
+            ArrayList<Point> points = new ArrayList<>();
+            points.add(pt);
+            poligons.put(numdibujo, points);
+        }
+        return pt;
     }
+
+    @MessageMapping("/queue.{numdibujo}")
+    public void handleQueueEvent(@DestinationVariable String numdibujo) throws Exception {
+        if(poligons.containsKey(numdibujo)){
+            msgt.convertAndSend("/topic/queue."+numdibujo, poligons.get(numdibujo));
+        }
+    }
+
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -9,7 +9,8 @@ var app = (function () {
     
     var stompClient = null;
 
-    var addPointToCanvas = function (point) {        
+
+    var addPointToCanvas = function (point) {
         var canvas = document.getElementById("canvas");
         var ctx = canvas.getContext("2d");
         ctx.beginPath();
@@ -19,7 +20,7 @@ var app = (function () {
     
     
     var getMousePosition = function (evt) {
-        canvas = document.getElementById("canvas");
+        var canvas = document.getElementById("canvas");
         var rect = canvas.getBoundingClientRect();
         var point ={
             x: evt.clientX - rect.left,
@@ -28,20 +29,52 @@ var app = (function () {
         stompClient.send("/topic/newpoint",{},JSON.stringify(point));
     };
 
+    var drawPolygon = function(points){
+            var canvas = document.getElementById("canvas");
+            var ctx = canvas.getContext("2d");
+            canvas.width = canvas.width;
+            var { x: fstPosX, y: fstPosY } = points[0];
+            ctx.moveTo(fstPosX, fstPosY);
 
-    var connectAndSubscribe = function () {
+            points.forEach(point => {
+                const { x , y } = point;
+                ctx.arc(x, y, 3, 0, 2 * Math.PI);
+                ctx.lineTo(x,y);
+            });
+            ctx.lineTo(fstPosX, fstPosY);
+            ctx.stroke();
+        }
+
+
+    var connectAndSubscribe = function (number) {
+        var canvas = document.getElementById("canvas");
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
         stompClient = Stomp.over(socket);
-        
+         canvas.width = canvas.width;
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+            stompClient.subscribe('/app/newpoint.'+number, function (eventbody) {
                var theObject=JSON.parse(eventbody.body);
                //alert(JSON.stringify(theObject));
                addPointToCanvas(theObject);
             });
+            stompClient.subscribe('/topic/newpolygon.'+number, function (eventbody) {
+                const points = JSON.parse(eventbody.body);
+                drawPolygon(points);
+            });
+
+        stompClient.subscribe('/topic/queue.'+number, function (eventbody) {
+            const points = JSON.parse(eventbody.body);
+            if( points.length  < 3 ){
+                points.forEach(point => addPointToCanvas(point));
+                return;
+            }
+            drawPolygon(points);
+        });
+
+        stompClient.send('/app/queue.'+number, {});
         });
 
     };
@@ -60,11 +93,13 @@ var app = (function () {
                                                       y: evt.clientY - rect.top
                                                   };
                                                   var pt = new Point(p.x,p.y);
-                                                  stompClient.send("/topic/newpoint",{},JSON.stringify(pt));
+                                                  console.log("Objeto con clase point: "+JSON.stringify(pt));
+                                                  console.log("Objeto sin clase point: "+JSON.stringify(p));
+                                                  stompClient.send("/app/newpoint."+$('#number').val(),{},JSON.stringify(pt));
                                               }, false);
             //websocket connection
-            connectAndSubscribe();
         },
+        connectAndSubscribe: connectAndSubscribe,
 
         publishPoint: function(px,py){
             var pt=new Point(px,py);
